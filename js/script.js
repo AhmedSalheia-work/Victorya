204cbcc28fe4f1d121c628d6ae6ca97476171549
AhmedSalheia <AhmedSalheia.as@gmail.com>
2020-09-03 13:22:41 +0300

    'EA

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 $(document).ready(function (){
 
+    $(window).on('scroll',function (){
+        if ($(window).innerWidth() <= 769)
+        {
+            if ($(window).scrollTop() >= window.innerHeight)
+            {
+                $('nav:nth-child(1)').addClass('scroll');
+            }else {
+                $('nav:nth-child(1)').removeClass('scroll');
+            }
+        }
+    });
+
     var swiper = new Swiper('.swiper-container', {
         slidesPerView: 'auto',
         centeredSlides: true,
